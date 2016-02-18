cf061f55ffc3a6b2400bfce8dad51a4f18c14256
Abhimanyu310 <abhip310@gmail.com>
2016-02-17 20:31:26 -0700

    Conference Central App

---
+++ settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+# WEB_CLIENT_ID = 'replace with Web client ID'
+# ANDROID_CLIENT_ID = 'replace with Android client ID'
+# IOS_CLIENT_ID = 'replace with iOS client ID'
+# ANDROID_AUDIENCE = WEB_CLIENT_ID
+
+
+WEB_CLIENT_ID = '658546505971-c3a87033pkfb6p45rm8111felspj53s2.apps.googleusercontent.com'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
+CLIENT_SECRET = 'wLVj2yBNo0oAFjcvIs6SjhYs'
